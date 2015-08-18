c31082f60640c1981cd2f7305c5dc7bb074f831f
liuhill <liu.lig@gmail.com>
2015-08-18 11:25:10 +0800

    创建照片墙controller

---
+++ modules/photowall/server/controllers/photowall.server.controller.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    _ = require('lodash');
+
+/**
+ * Create a Photowall
+ */
+exports.create = function(req, res) {
+
+};
+
+/**
+ * Show the current Photowall
+ */
+exports.read = function(req, res) {
+
+};
+
+/**
+ * Update a Photowall
+ */
+exports.update = function(req, res) {
+
+};
+
+/**
+ * Delete an Photowall
+ */
+exports.delete = function(req, res) {
+
+};
+
+/**
+ * List of Photowalls
+ */
+exports.list = function(req, res) {
+
+};
